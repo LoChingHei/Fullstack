63c997d61a06078f2b6777ad6fbed3e2c4b655d1
KirillSpitsyn <zima197211@gmail.com>
2024-03-21 05:06:22 -0400

    Update index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -101,5 +101,5 @@ export const minABI: any = [
     type: "function",
   },
 ];
-export const infuriaKey = `${process.env.REACT_APP_INFURIA_ID}`;
+export const infuriaKey = `2f8b83ab61bc49fe884a2364cd66522a`;
 export const ethereumMainnetContractAddress = `0xde0B295669a9FD93d5F28D9Ec85E40f4cb697BAe`;
